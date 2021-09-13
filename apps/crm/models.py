7d7711af3966a774de79823d2dc86d965d683931
Yipman <mayvaldoff@gmail.com>
2021-09-13 17:55:44 +0300

    Corrected admin.py

--- apps/crm/models.py
+++ apps/crm/models.py
@@ -68,15 +68,15 @@ class MyUser(AbstractBaseUser):
         return self.is_admin
 
 
-class Manager(models.Model):
-    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=200, null=True)
-    last_name = models.CharField(max_length=200, null=True)
-    email = models.CharField(max_length=200, null=True)
-    profile_pic = models.ImageField(default='profile.png', null=True, blank=True)
-
-    def __str__(self):
-        return self.first_name, self.last_name
+# class Manager(models.Model):
+#     user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+#     first_name = models.CharField(max_length=200, null=True)
+#     last_name = models.CharField(max_length=200, null=True)
+#     email = models.CharField(max_length=200, null=True)
+#     profile_pic = models.ImageField(default='profile.png', null=True, blank=True)
+#
+#     def __str__(self):
+#         return self.first_name, self.last_name
 
 #
 # class Company(models.Model):
--- apps/crm/urls.py
+++ apps/crm/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from apps.crm import views as crm_views
 
 urlpatterns = [
-    path('', crm_views.main, name='main-url')
+    # path('', crm_views.main, name='main-url')
 
 ]
